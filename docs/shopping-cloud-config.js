66b2722140bfb9dc36a0fdf25fc215fc63ce5939
lachlanmcnie-netizen <lachlanmcnie@gmail.com>
2026-04-01 06:43:55 +1300

    Add files via upload

---
+++ docs/shopping-cloud-config.js
@@ -0,0 +1,7 @@
+window.SHOPPING_CLOUD_CONFIG = {
+  supabaseUrl: "https://rlhnfdratufyqipncuzz.supabase.co",
+  supabaseAnonKey: "sb_publishable_WCO0Vnz6H4V1H5bJX8o3uQ_tW_QSrzQ",
+  vapidPublicKey: "YOUR_VAPID_PUBLIC_KEY",
+  pushFunctionUrl: "https://YOUR_PROJECT.functions.supabase.co/send-shopping-push",
+  recipeScanFunctionUrl: "https://rlhnfdratufyqipncuzz.supabase.co/functions/v1/scan-shopping-recipe"
+};
